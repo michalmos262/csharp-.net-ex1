ba42936a0137ee96608c6bfcaf7622c6234b6795
Michal Moscovich <michal.mos262@gmail.com>
2024-04-25 22:55:09 +0300

    wip

---
+++ Ex01_03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex01_03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int diamondHeight = GetDiamondHeightFromUser();
+            Ex01_02.Program.DrawDiamond(diamondHeight);
+        }
+
+        static int GetDiamondHeightFromUser()
+        {
+            string userInput;
+            bool isInputInteger;
+
+            Console.WriteLine("Please enter the diamond height:");
+            userInput = Console.ReadLine();
+            isInputInteger = int.TryParse(userInput, out int diamondHeight);
+            while (!isInputInteger || diamondHeight <= 0)
+            {
+                Console.WriteLine("Wrong input! Diamond height must be a positive whole number, please try again:");
+                userInput = Console.ReadLine();
+                isInputInteger = int.TryParse(userInput, out diamondHeight);
+            }
+            return diamondHeight;
+        }
+    }
+}
